de99e944ef67c6d3aa1a32be36b726308d37935a
tanapornPipat <tanaporn.pipa@gmail.com>
2018-05-26 22:19:42 +0700

    pgp

--- src/Recipient.java
+++ src/Recipient.java
@@ -7,6 +7,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.Base64;
 
 public class Recipient {
 
@@ -25,40 +26,60 @@ public class Recipient {
 
 	public void read(String algorithm) {
 		
-		//Radix-64 conversion
-		
 		text = readFile("email");
 		
-		switch (algorithm) {
-			case "SHA-256":
-				try {
-					hash = sha256(text);
-					checksum = readFile("checksum");
-					validate(hash, checksum);
-				} catch (NoSuchAlgorithmException e) {
-					e.printStackTrace();
+		//Radix-64 conversion
+		byte[] decodedBytes = Base64.getDecoder().decode(text);
+		String message = new String(decodedBytes);
+		
+		/*
+		 *  message = sessionKeyComponent + "\n" + dsComponent + "\n" + cipher 	
+		 */
+		String[] component = message.split("\n");
+		
+		int num = component.length;
+		if (num == 3) {
+			
+			String[] sessionKeyComponent = component[0].split(",");
+			String[] dsComponent = component[1].split(",");
+			String cipher = component[2];
+			
+			
+			
+			switch (algorithm) {
+				case "SHA-256":
+					try {
+						hash = sha256(text);
+						checksum = readFile("checksum");
+						validate(hash, checksum);
+					} catch (NoSuchAlgorithmException e) {
+						e.printStackTrace();
+					}
+					break;
+				case "MD5":
+					try {
+						hash = md5(text);
+						checksum = readFile("checksum");
+						validate(hash, checksum);
+					} catch (NoSuchAlgorithmException e) {
+						e.printStackTrace();
+					}
+					break;
+				case "SHA-1":
+					try {
+						hash = sha1(text);
+						checksum = readFile("checksum");
+						validate(hash, checksum);
+					} catch (NoSuchAlgorithmException e) {
+						e.printStackTrace();
+					}
+				default:
+					break;
 				}
-				break;
-			case "MD5":
-				try {
-					hash = md5(text);
-					checksum = readFile("checksum");
-					validate(hash, checksum);
-				} catch (NoSuchAlgorithmException e) {
-					e.printStackTrace();
-				}
-				break;
-			case "SHA-1":
-				try {
-					hash = sha1(text);
-					checksum = readFile("checksum");
-					validate(hash, checksum);
-				} catch (NoSuchAlgorithmException e) {
-					e.printStackTrace();
-				}
-			default:
-				break;
-			}
+		}
+		else {
+			System.out.println("Error!");
+		}
 	}
 
 	public String readFile(String filename) {
