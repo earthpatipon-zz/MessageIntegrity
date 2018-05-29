9d5a318f43440e22c014efb561970b92273cd4a0
earthpatipon <eartyearth@gmail.com>
2018-05-29 11:56:39 +0700

    final

---
+++ src/Recipient_GUI.java
@@ -0,0 +1,202 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import java.awt.SystemColor;
+import java.awt.Label;
+import java.awt.Color;
+import javax.swing.JSlider;
+import java.awt.Canvas;
+import java.awt.TextArea;
+import java.awt.ScrollPane;
+import javax.swing.JScrollPane;
+import java.awt.Panel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.util.Base64;
+
+import javax.swing.JTextArea;
+import javax.swing.event.AncestorListener;
+import javax.swing.event.AncestorEvent;
+
+public class Recipient_GUI extends JFrame {
+
+	private JPanel contentPane;
+	private String decrypt="";
+	private static String text;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					text = readFile("email");
+					Recipient_GUI frame = new Recipient_GUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public Recipient_GUI() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+		
+		Recipient rp = new Recipient();
+		String algorithm = rp.readFile("algorithm");
+		if(algorithm != "None") {
+			PrivateKey privatekey = rp.getPrivateKey();
+			rp.read(algorithm, privatekey);
+			decrypt = rp.getHash();
+		}
+		
+		String[] message = rp.readFile("email").split(",");
+		
+		rp.setName(message[0]);
+		text = message[1];
+		if (message.length > 2) {
+			for (int i = 2; i < message.length; i++) {
+				text += "," + message[i];
+			}
+		}
+		initialize(rp, text);
+	}
+	
+	private void initialize(Recipient rp, String text) {
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 672, 578);
+		contentPane = new JPanel();
+		contentPane.addAncestorListener(new AncestorListener() {
+			public void ancestorAdded(AncestorEvent arg0) {
+				if (!rp.readFile("algorithm").equals("None")) {
+					String warning = rp.validate(rp.getHash(), rp.readFile("checksum"));
+					if (warning.equals("Integrity lost!"))
+					{
+						JOptionPane.showMessageDialog(null, warning, "Warning", JOptionPane.ERROR_MESSAGE);
+					}
+					else
+						JOptionPane.showMessageDialog(null, warning, "Confirm message", JOptionPane.INFORMATION_MESSAGE);
+				}
+			}
+			public void ancestorMoved(AncestorEvent arg0) {
+			}
+			public void ancestorRemoved(AncestorEvent arg0) {
+			}
+		});
+		contentPane.setBackground(SystemColor.info);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		setLocationRelativeTo(null); 
+
+		Label label_2 = new Label("Sent Items");
+		label_2.setFont(new Font("Dialog", Font.PLAIN, 18));
+		label_2.setBackground(new Color(255, 255, 255));
+		label_2.setAlignment(Label.CENTER);
+		label_2.setBounds(0, 83, 163, 24);
+		contentPane.add(label_2);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(178, 64, 464, 22);
+		contentPane.add(separator);
+		
+		JLabel lblNewLabel = new JLabel("Receiver Mailbox");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 30));
+		lblNewLabel.setBounds(189, 14, 271, 37);
+		contentPane.add(lblNewLabel);
+		
+		Label label = new Label("Inbox 1");
+		label.setAlignment(Label.CENTER);
+		label.setBackground(new Color(176, 224, 230));
+		label.setFont(new Font("Dialog", Font.BOLD, 18));
+		label.setBounds(0, 42, 163, 24);
+		contentPane.add(label);
+		
+		Label label_3 = new Label("Junk Email");
+		label_3.setFont(new Font("Dialog", Font.PLAIN, 18));
+		label_3.setBackground(Color.WHITE);
+		label_3.setAlignment(Label.CENTER);
+		label_3.setBounds(0, 123, 163, 24);
+		contentPane.add(label_3);
+		
+		Label label_4 = new Label(rp.getName());
+		label_4.setFont(new Font("Dialog", Font.PLAIN, 20));
+		label_4.setBackground(SystemColor.info);
+		label_4.setBounds(271, 113, 293, 24);
+		contentPane.add(label_4);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		Image img = new ImageIcon (this.getClass().getResource("user.png")).getImage();
+		lblNewLabel_1.setIcon(new ImageIcon(img));
+		lblNewLabel_1.setBounds(194, 103, 62, 64);
+		contentPane.add(lblNewLabel_1);
+		
+		Label label_5 = new Label("Tue 5/22, 10:30 AM");
+		label_5.setFont(new Font("Dialog", Font.PLAIN, 16));
+		label_5.setBackground(SystemColor.info);
+		label_5.setBounds(271, 143, 293, 24);
+		contentPane.add(label_5);
+		
+		Label label_1 = new Label("");
+		label_1.setBackground(SystemColor.text);
+		label_1.setBounds(0, 0, 163, 531);
+		contentPane.add(label_1);
+		
+		TextArea textArea = new TextArea();
+		textArea.setFont(new Font("Dialog", Font.PLAIN, 20));
+		textArea.setText(text);
+		textArea.setEditable(false);
+		textArea.setBounds(189, 204, 440, 295);
+		contentPane.add(textArea);
+	}
+	public static String readFile(String filename) {
+		BufferedReader br = null;
+		FileReader fr = null;
+		StringBuilder sb = null;
+		String currentLine = "";
+
+		try {
+			fr = new FileReader("inbox/" + filename + ".txt");
+			br = new BufferedReader(fr);
+			sb = new StringBuilder();
+			
+			while ((currentLine = br.readLine()) != null) {
+				sb.append(currentLine);
+			}
+//			String[] message = sb.toString().split(",");
+//			sb.setLength(0); 
+//			sb.append(message[1]);
+//			if (message.length > 2) {
+//				for (int i = 2; i < message.length; i++) {
+//					sb.append("," + message[i]);
+//				}
+//			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return sb.toString();
+	}
+}
--- src/Sender_GUI.java
+++ src/Sender_GUI.java
@@ -1,49 +1,60 @@
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.Font;
-import java.awt.Image;
+
+import javax.swing.JFrame;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+
+import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
+import javax.swing.Box;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
-import javax.swing.border.EmptyBorder;
-import java.awt.SystemColor;
-import java.awt.Label;
-import java.awt.Color;
-import javax.swing.JSlider;
-import java.awt.Canvas;
-import java.awt.TextArea;
-import java.awt.ScrollPane;
-import javax.swing.JScrollPane;
 import java.awt.Panel;
+import java.awt.Label;
+import java.awt.SystemColor;
+import java.awt.Font;
+import java.awt.TextField;
+import java.awt.Toolkit;
+
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Button;
+import javax.swing.UIManager;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.Base64;
+import java.util.Scanner;
 
-import javax.swing.JTextArea;
-import javax.swing.event.AncestorListener;
-import javax.swing.event.AncestorEvent;
+import javax.swing.JSeparator;
+import java.awt.List;
+import javax.swing.JList;
+import java.awt.ScrollPane;
+import javax.swing.JScrollPane;
 
-public class Sender_GUI extends JFrame {
+public class Sender_GUI {
 
-	private JPanel contentPane;
-	private String decrypt="";
-	private static String text;
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_2;
+	private String word;
 	
+
 	/**
 	 * Launch the application.
 	 */
@@ -51,9 +62,8 @@ public class Sender_GUI extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					text = readFile("email");
-					Sender_GUI frame = new Sender_GUI();
-					frame.setVisible(true);
+					Sender_GUI window = new Sender_GUI();
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -62,148 +72,148 @@ public class Sender_GUI extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Create the application.
 	 */
-//	public Sender_GUI(String text) {
-//		initialize(text);
-//	}
-	
-	public Sender_GUI() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
-		
-		Recipient rp = new Recipient();
-		String algorithm = rp.readFile("algorithm");
-		if(algorithm != "None") {
-			PrivateKey privatekey = rp.getPrivateKey();
-			rp.read(algorithm, privatekey);
-			decrypt = rp.getHash();
-		}
-		
-		String[] message = rp.readFile("email").split(",");
-		
-		rp.setName(message[0]);
-		text = message[1];
-		if (message.length > 2) {
-			for (int i = 2; i < message.length; i++) {
-				text += "," + message[i];
-			}
-		}
-		initialize(rp, text);
+	public Sender_GUI() {
+		initialize();
 	}
-	
-	private void initialize(Recipient rp, String text) {
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 672, 578);
-		contentPane = new JPanel();
-		contentPane.addAncestorListener(new AncestorListener() {
-			public void ancestorAdded(AncestorEvent arg0) {
-				if (!rp.readFile("algorithm").equals("None")) {
-					String warning = rp.validate(rp.getHash(), rp.readFile("checksum"));
-					if (warning.equals("Integrity lost!"))
-					{
-						JOptionPane.showMessageDialog(null, warning, "Warning", JOptionPane.ERROR_MESSAGE);
-					}
-					else
-						JOptionPane.showMessageDialog(null, warning, "Confirm message", JOptionPane.INFORMATION_MESSAGE);
-				}
-			}
-			public void ancestorMoved(AncestorEvent arg0) {
-			}
-			public void ancestorRemoved(AncestorEvent arg0) {
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(SystemColor.info);
+		frame.setBounds(100, 100, 672, 578);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+
+		Label label = new Label("To");
+		label.setFont(new Font("Dialog", Font.BOLD, 12));
+		label.setAlignment(Label.CENTER);
+		label.setBackground(SystemColor.menu);
+		label.setBounds(202, 92, 70, 37);
+		frame.getContentPane().add(label);
+
+		textField_2 = new JTextField();
+		textField_2.setBackground(SystemColor.window);
+		textField_2.setUI(new JTextFieldHintUI("Add a subject", Color.gray));
+		textField_2.setBounds(202, 165, 435, 37);
+		frame.getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+
+		textField = new JTextField();
+		textField.setBounds(278, 92, 359, 37);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+
+		JTextArea textArea = new JTextArea();
+		textArea.setRows(10);
+		textArea.setBounds(202, 241, 435, 204);
+		frame.getContentPane().add(textArea);
+		textArea.setUI(new JTextFieldHintUI("Add a message", Color.gray));
+
+		Button button = new Button("Send");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		contentPane.setBackground(SystemColor.info);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		setLocationRelativeTo(null); 
 
-		Label label_2 = new Label("Sent Items");
-		label_2.setFont(new Font("Dialog", Font.PLAIN, 18));
-		label_2.setBackground(new Color(255, 255, 255));
-		label_2.setAlignment(Label.CENTER);
-		label_2.setBounds(0, 83, 163, 24);
-		contentPane.add(label_2);
-		
+		button.setForeground(Color.WHITE);
+		button.setBackground(SystemColor.textHighlight);
+		button.setFont(new Font("Dialog", Font.BOLD, 12));
+		button.setBounds(530, 477, 90, 37);
+		frame.getContentPane().add(button);
+
 		JSeparator separator = new JSeparator();
-		separator.setBounds(178, 64, 464, 22);
-		contentPane.add(separator);
-		
-		JLabel lblNewLabel = new JLabel("Receiver Mailbox");
+		separator.setBounds(21, 64, 616, 22);
+		frame.getContentPane().add(separator);
+
+		JLabel lblNewLabel = new JLabel("Sender Mailbox");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 30));
 		lblNewLabel.setBounds(189, 14, 271, 37);
-		contentPane.add(lblNewLabel);
-		
-		Label label = new Label("Inbox 1");
-		label.setAlignment(Label.CENTER);
-		label.setBackground(new Color(176, 224, 230));
-		label.setFont(new Font("Dialog", Font.BOLD, 18));
-		label.setBounds(0, 42, 163, 24);
-		contentPane.add(label);
-		
-		Label label_3 = new Label("Junk Email");
-		label_3.setFont(new Font("Dialog", Font.PLAIN, 18));
-		label_3.setBackground(Color.WHITE);
-		label_3.setAlignment(Label.CENTER);
-		label_3.setBounds(0, 123, 163, 24);
-		contentPane.add(label_3);
-		
-		Label label_4 = new Label(rp.getName());
-		label_4.setFont(new Font("Dialog", Font.PLAIN, 20));
-		label_4.setBackground(SystemColor.info);
-		label_4.setBounds(271, 113, 293, 24);
-		contentPane.add(label_4);
-		
-		JLabel lblNewLabel_1 = new JLabel("");
-		Image img = new ImageIcon (this.getClass().getResource("/user.png")).getImage();
-		lblNewLabel_1.setIcon(new ImageIcon(img));
-		lblNewLabel_1.setBounds(194, 103, 62, 64);
-		contentPane.add(lblNewLabel_1);
-		
-		Label label_5 = new Label("Tue 5/22, 10:30 AM");
-		label_5.setFont(new Font("Dialog", Font.PLAIN, 16));
-		label_5.setBackground(SystemColor.info);
-		label_5.setBounds(271, 143, 293, 24);
-		contentPane.add(label_5);
-		
-		Label label_1 = new Label("");
-		label_1.setBackground(SystemColor.text);
-		label_1.setBounds(0, 0, 163, 531);
-		contentPane.add(label_1);
-		
-		TextArea textArea = new TextArea();
-		textArea.setFont(new Font("Dialog", Font.PLAIN, 20));
-		textArea.setText(text);
-		textArea.setEditable(false);
-		textArea.setBounds(189, 204, 440, 295);
-		contentPane.add(textArea);
-	}
-	public static String readFile(String filename) {
-		BufferedReader br = null;
-		FileReader fr = null;
-		StringBuilder sb = null;
-		String currentLine = "";
+		frame.getContentPane().add(lblNewLabel);
 
-		try {
-			fr = new FileReader("inbox/" + filename + ".txt");
-			br = new BufferedReader(fr);
-			sb = new StringBuilder();
-			
-			while ((currentLine = br.readLine()) != null) {
-				sb.append(currentLine);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(21, 92, 153, 353);
+		frame.getContentPane().add(scrollPane);
+
+		JList list = new JList();
+		list.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		scrollPane.setViewportView(list);
+		DefaultListModel suggestList = new DefaultListModel();
+		suggestList.addElement("MD5");
+		suggestList.addElement("SHA-1");
+		suggestList.addElement("SHA-256");
+		suggestList.addElement("Asymmetric Encryption");
+		suggestList.addElement("None");
+		list.setModel(suggestList);
+
+		// click list
+		list.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent mouse) {
+				int point;
+				int count = mouse.getClickCount();
+				JList theList = (JList) mouse.getSource();
+				if (count == 2) // double click
+				{
+					point = theList.locationToIndex(mouse.getPoint());
+					if (point >= 0) {
+						Object object = theList.getModel().getElementAt(point);
+						word = object.toString();
+						System.out.println("Your selected algorithm is " + word);
+
+					}
+				}
 			}
-//			String[] message = sb.toString().split(",");
-//			sb.setLength(0); 
-//			sb.append(message[1]);
-//			if (message.length > 2) {
-//				for (int i = 2; i < message.length; i++) {
-//					sb.append("," + message[i]);
-//				}
-//			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return sb.toString();
+		});
+
+		button.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+
+				Sender sender = new Sender();
+				Recipient recipient = new Recipient();
+				
+				PrivateKey privateKey = null;
+				PublicKey publicKey = null;
+				KeyPairGenerator keyGen;
+				
+				try {
+					if (word.equals("Asymmetric Encryption")) {
+						keyGen = KeyPairGenerator.getInstance("RSA");
+						keyGen.initialize(1024);
+						KeyPair keyPair = keyGen.genKeyPair();
+						publicKey = keyPair.getPublic();
+						privateKey = keyPair.getPrivate();
+						recipient.setPrivateKey(privateKey);
+					}
+				} catch (NoSuchAlgorithmException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+				try {
+					sender.send(word, textField.getText() + "," + textArea.getText(), publicKey);
+					sender.writeFile("algorithm", word);
+					if(!word.equals("None")) {
+						String encrypt = sender.getHash();
+						JOptionPane.showMessageDialog(null, "Encrypted message with " + word + " :\n"
+								+ encrypt, "Send Completed", JOptionPane.INFORMATION_MESSAGE);
+					}
+					else {
+						JOptionPane.showMessageDialog(null, "Send Completed", "", JOptionPane.INFORMATION_MESSAGE);
+					}
+				} catch (InvalidKeyException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException
+						| NoSuchAlgorithmException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 }
